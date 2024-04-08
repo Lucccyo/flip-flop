511fe2aef4480d05aea6e1382a5b9f01e6403c08
Lucccyo <cha.git@mailo.fr>
2024-04-08 19:24:41 +0200

    add new words

--- data/ex_cambridge.js
+++ data/ex_cambridge.js
@@ -90,7 +90,7 @@ export const sentences = [
     english: "Put the lights on, will you? It's getting dark.",
   },
   {
-      type: "cambridge 1",
+    type: "cambridge 1",
     french: "Le théâtre du coin monte une production de Hamlet.",
     english: "They're putting on a production of Hamlet at the local theatre.",
   },
@@ -174,7 +174,7 @@ export const sentences = [
     french: "La douleur d'un calcul rénal est atroce.",
     english: "The pain of a heart operation is excruciating.",
   },
-// ____________________________________2________________________________________
+  // ____________________________________2________________________________________
   {
     type: "cambridge 2",
     french: "	Le chien eut l'air malheureux après avoir été grondé par son maître.",
@@ -323,5 +323,338 @@ export const sentences = [
     type: "cambridge 2",
     french: "	Je vous présente mes sincères salutations.",
     english: "	You have my genuine thanks.",
+  },
+
+  // ____________________________________english lesson________________________________________
+
+  {
+    type: "english lesson 08/04",
+    french: "	Veuillez voir plus bas pour plus de détails.",
+    english: "	Please see underneath for further details.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Je me rappelle mes années à l'école et je souris.",
+    english: "	I look back on my years in school and smile.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "",
+    english: "	Rather than look back, I must remind myself to look towards the future to better days.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "Ne te retourne pas. Quoi que ce soit qui te pourchasse te rattrape peut-être.",
+    english: "Don't look back. Whatever's chasing you might be gaining on you",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Nous allons à la salle de sport faire de la muscu cet après-midi.",
+    english: "	We're going to the gym to work out this afternoon.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "",
+    english: "	I'm still trying to work out this last crossword clue.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "",
+    english: "	Marc is a moody character; I can't work him out.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Tous les autres enfants ont été invités à la fête, mais on m'a laissé à l'écart.",
+    english: "	All the other children were invited to the party, but I was left out.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Le pain n'a pas levé parce que j'ai oublié la levure.",
+    english: "	The bread did not rise because I left out the yeast by mistake.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	La banque a refusé ma demande de crédit.",
+    english: "	The bank turned down my application for a loan.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	J'aimerais bien que tu baisses ta musique !",
+    english: "	I wish you'd turn down that music!",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	La femme de chambre de l'hôtel a ouvert le lit et laissé un chocolat sur l'oreiller.",
+    english: "	The hotel cleaner had turned down the sheets and left a chocolate on the pillow.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Je refuse de supporter tes pleurnicheries plus longtemps. Va au lit tout de suite !",
+    english: "	I will not put up with your whining any more. Go to bed this minute!",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Je ne veux pas te décourager, mais la marque de voiture que tu veux acheter est très dure à entretenir.",
+    english: "	I don't want to put you off, but that make of car you're thinking of buying is very hard to maintain.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Je suis occupée cet après-midi, pouvons-nous reporter notre réunion à demain ?",
+    english: "	I'm busy this afternoon; can we put our meeting off until tomorrow?",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	C'était répugnant, ça m'a dégoûté de mon repas.",
+    english: "	That was disgusting; it has put me off my dinner.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	J'ai du mal à réduire ma consommation d'alcool quand mes amis n'arrêtent pas de m'inviter à boire des coups.",
+    english: "	It's hard to cut down on alcohol when my friends keep inviting me out for drinks.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	C'est dommage qu'ils abattent ce vieil arbre.",
+    english: "	It's a pity that they cut down that old tree.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Nous avons été forcés d'augmenter nos prix pour couvrir les coûts des matières premières.",
+    english: "	We've been forced to put up our prices to cover the costs of raw materials.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Il a accroché une photo pour la montrer aux visiteurs.",
+    english: "	He put a picture up for visitors to see.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Nous serions heureux de vous héberger la prochaine fois que vous passerez par Londres.",
+    english: "	We would be happy to put you up when you next come to London.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Levez la main si vous connaissez la réponse.",
+    english: "	Put your hand up if you know the answer.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Patsy pose ses stylos sur le bureau.",
+    english: "	Patsy put her pens down on the desk.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Tu ne devrais pas le rabaisser comme ça.",
+    english: "	You shouldn't put him down like that.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Le vétérinaire a dû piquer notre cochon d'Inde parce qu'il était très malade.",
+    english: "	The vet had to put our guinea pig down because she was very sick.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Il s'est mis à la menuiserie après avoir hérité des outils de son grand-père.",
+    english: "	He took up woodworking after inheriting his grandfather's tools.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Ce projet va prendre pas mal de ton temps.",
+    english: "	That project will take up most of your time.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Les campeurs ont démonté leur tente et l'ont mise dans la voiture.",
+    english: "	The campers took the tent down and packed it away in the car.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Le couple s'est séparé après trois ans de relation.",
+    english: "	The couple broke up after a three-year relationship.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	La roche se décompose petit à petit en sable.",
+    english: "	Rock gradually breaks up into sand.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	L'école finit la semaine prochaine pour les vacances d'été.",
+    english: "	School breaks up next week for the summer holidays.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	La voiture est tombée en panne sur le chemin du retour.",
+    english: "	The car broke down on the way home.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Stella a fondu en larmes quand la police l'a informée de l'accident de son mari.",
+    english: "	Stella broke down when the police told her about her husband's accident.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	La police a enfoncé la porte quand ils ont fait une descente dans la maison.",
+    english: "	The police broke down the door when they raided the house.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Jane et moi économisons pour nous marier.",
+    english: "	Jane and I are saving up to get married.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Mets ton linge à laver dans le panier.",
+    english: "	Put your laundry in the hamper.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "",
+    english: "	George must have had some good news; he looks very pleased.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: " La circulation était tellement mauvaise que lorsque je suis arrivé au bureau, j'avais 20 minutes de retard.",
+    english: "The traffic was so bad that by the time I got to the office I was 20 minutes late.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Sors de l'ombre et mets-toi ici dans la lumière pour que je te voie.",
+    english: "Come out of the shadows and stand here in the light where I can see you.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Si cette affaire est rendue publique, il sera ruiné.",
+    english: "	If news of the affair comes out he will be ruined.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Mon grand-père mélange toujours ses mots.",
+    english: "	My grandfather is always mixing up his words.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Je vais concocter quelques milkshakes à la fraise.",
+    english: "	I'm going to mix up some strawberry milkshakes.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Est-ce que Lola et Archie sont amis ou est-ce qu'ils sortent ensemble ?",
+    english: "	Are Lola and Archie just friends, or are they going out?",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Pourrais-tu mettre une autre bûche dans le feu avant qu'il ne s'éteigne ?",
+    english: "	Could you put another log on the fire, please, before it goes out.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "",
+    english: "Chaque jour, des centaines de clients franchissent le seuil de ce restaurant.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "Conduis avec prudence pour arriver tranquillement.",
+    english: "	Drive carefully so that you will arrive safely.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Il a soigneusement inspecté le restaurant pour s'assurer que celui-ci n'enfreignait aucun code.",
+    english: "	The inspector looked over the restaurant for code violations.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Elle savait que son frère avait commis un crime mais elle refusait de le dénoncer.",
+    english: "	She knew her brother had committed a crime, but she refused to turn him in.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Je pensais que l'accord serait très profitable pour mes affaires mais il est tombé à l'eau à la dernière minute.",
+    english: "	I thought that the deal would be very profitable for my business, but it fell through at the last minute.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "",
+    english: "	All the factors were in place to catch him but somehow he managed to fall through the net.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Ça va être dur, mais je vais essayer de renoncer au chocolat pour le Carême.",
+    english: "	It'll be hard, but I'm going to try giving up chocolate for Lent.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Prenez le métro et descendez à Union Station.",
+    english: "	Take the subway and get off at Union Station.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	J'ai besoin de faire un emprunt à la banque.",
+    english: "	I need to get a loan from the bank.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Kathy a emprunté une jupe à sa sœur pour la porter à la soirée.",
+    english: "	Kathy borrowed a skirt from her sister to wear to the party.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Si tu as chaud, pourquoi est-ce que tu n'enlèves pas ton pull ?",
+    english: "	If you're hot, why not take off that jumper?",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	L'avion a décollé après un bref retard.",
+    english: "	The plane took off after a short delay.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Ouah, ces couleurs vives ressortent vraiment.",
+    english: "	Wow, those bright colors really stand out.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Carl a allumé la radio pour écouter les informations.",
+    english: "	Carl turned on the radio to listen to the news.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Je ne m'attendais pas à ce qu'il se pointe à ma fête vu qu'il n'était pas invité.",
+    english: "	I didn't expect him to turn up at my party as he wasn't invited.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "Je perds souvent mes clés et généralement, elles réapparaissent dans la cuisine.",
+    english: "	I often lose my keys but they usually turn up somewhere in the kitchen.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Nous avons monté le son de la télé pour couvrir le bruit des voisins qui se disputaient.",
+    english: "	We turned the TV up to drown the noise of our neighbours arguing.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Peu d'électeurs sont venus voter le jour de l'élection.",
+    english: "	Not many people turned out to vote on election day.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "s'averer",
+    english: "	I thought Isabelle was American, but it turns out she's from Canada.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "",
+    english: "	Gina turned out her handbag and rummaged through her things for the car keys.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "	Rufus s'est mis à battre le tempo à la batterie.",
+    english: "	Rufus began beating out a rhythm on the drums.",
+  },
+  {
+    type: "english lesson 08/04",
+    french: "",
+    english: "	Tom couldn't see through the dense fog.",
   },
 ]
